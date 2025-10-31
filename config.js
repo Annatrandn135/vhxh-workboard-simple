21289f8dc3f89060564ffe26f96af4832f203fed
Annatrandn135 <annatran.dn135@gmail.com>
2025-10-31 18:38:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
-const LINKS = {
-  lichUBND: "https://docs.google.com/spreadsheets/d/xxxx",
-  lichPhong: "https://docs.google.com/spreadsheets/d/yyyy",
+window.LINKS = {
+  lichUBND: "https://docs.google.com/spreadsheets/d/XXXXXXX/view?usp=sharing",
+  lichPhong: "https://docs.google.com/spreadsheets/d/YYYYYYY/view?usp=sharing",
   trongTam: "https://docs.google.com/spreadsheets/d/1-_Xp3ff9PmiDL1xAqsuaxkodn5XwJo84MOwbiFTqMCE/edit?gid=1620608222#gid=1620608222",
-  cbcc: "https://docs.google.com/spreadsheets/d/aaaa",
-  baoCao: "https://docs.google.com/document/d/bbbb"
+  cbcc:     "https://docs.google.com/spreadsheets/d/AAAAAAA/view?usp=sharing",
+  baoCao:   "https://docs.google.com/document/d/BBBBBBB/view?usp=sharing"
 };
