54d5e93109b7258a530e3628de0d504afe0f686e
Annatrandn135 <annatran.dn135@gmail.com>
2025-10-31 18:21:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const LINKS = {
   lichUBND: "https://docs.google.com/spreadsheets/d/xxxx",
   lichPhong: "https://docs.google.com/spreadsheets/d/yyyy",
-  trongTam: "https://docs.google.com/spreadsheets/d/zzzz",
+  trongTam: "https://docs.google.com/spreadsheets/d/1-_Xp3ff9PmiDL1xAqsuaxkodn5XwJo84MOwbiFTqMCE/edit?gid=1620608222#gid=1620608222",
   cbcc: "https://docs.google.com/spreadsheets/d/aaaa",
   baoCao: "https://docs.google.com/document/d/bbbb"
 };
